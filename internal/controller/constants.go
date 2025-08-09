15fea9d96ebcc307ccf139a5f72cc74591c85a4e
MohammadHosein Bahmani <mhossein.bahmani@divar.ir>
2025-08-09 17:48:27 +0330

    Add locking mechanism for LV operations
    Signed-off-by: MohammadHosein Bahmani <mhbahmani79@gmail.com>

--- internal/controller/constants.go
+++ internal/controller/constants.go
@@ -11,4 +11,5 @@ const (
 	// be dynamically provisioned. Its value is the name of the selected node.
 	// https://github.com/kubernetes/kubernetes/blob/9bae1bc56804db4905abebcd408e0f02e199ab93/pkg/controller/volume/persistentvolume/util/util.go#L53
 	AnnSelectedNode = "volume.kubernetes.io/selected-node"
+	LvmLockName     = "topolvm-lv-lock"
 )
